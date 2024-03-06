3dc5fb8dca951e596292a838e031845cb3a5c53a
Oluwakemi <oluwakemie05@gmail.com>
2024-03-06 15:31:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="bg-white relative top-0 text-white">
+        <img src="group.jpg" className="w-full h-screen object-cover" alt="" />
+        <nav className="flex justify-around items-center absolute top-0 w-full mx-auto">
+          <div>
+            <img src="log.png" className="w-[120px]" alt="" />
+          </div>
+          <div>
+            <ul className="flex items-center gap-[4vw] font-bold">
+              <li>
+                <a className="hover:text-pink-500" href="">
+                  HOME
+                </a>
+              </li>
+              <li>
+                <a className="hover:text-white " href="">
+                  COUPLE
+                </a>
+              </li>
+              <li>
+                <a className="hover:text-white " href="">
+                  STORY
+                </a>
+              </li>
+              <li>
+                <a className="hover:text-white" href="">
+                  RSVP
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>{" "}
+        <div className="absolute flex flex-col justify-center items-center inset-0">
+          <p className="text-3xl md:text-5xl lg:text-7xl font-bold">
+            CYNTHIAN Hooks AYOMIBO
+          </p>
+          <p className="text-2xl">12-05-2024</p>
+          <i className="text-2xl md:text-4xl">#CAaffairs</i>
+        </div>
       </header>
-    </div>
+      <div className="p-5 md:p-10">
+        <div className="grid grid-cols-3 grid-rows-5 gap-4">
+          <div>
+            {" "}
+            <img src="asoebi.jpg" alt="" className="row-span-2 h-full w-full object-cover" />
+          </div>
+          <div className="row-span-2">
+            {" "}
+            <img src="asoebi1.jpeg" alt="" className="row-span-2 h-full w-full object-cover" />
+          </div>
+          <div>
+            {" "}
+            <img src="asoebi2.jpeg" alt="" className="row-span-2 h-full w-full object-cover" />
+          </div>
+          <div className="col-start-2 row-start-3">
+            {" "}
+            <img src="asoebi3.jpg" alt="" className="row-span-2 h-full w-full object-cover" />
+          </div>
+          <div className="row-span-2 col-start-1 row-start-2">
+            {" "}
+            <img src="asoebi4.jpg" alt="" className="row-span-2 h-full w-full object-cover" />
+          </div>
+          <div className="row-span-2 col-start-3 row-start-2">
+            {" "}
+            <img src="asoebi11.jpg" alt="" className="row-span-2 h-full w-full object-cover" />
+          </div>
+          <div className="col-span-2 row-start-4">
+            {" "}
+            <img src="asoebi6.png" alt="" className="row-span-2 h-full w-full object-cover" />
+          </div>
+          <div className="col-start-3 row-start-4">
+            {" "}
+            <img src="asoebi7.png" alt="" className="row-span-2 h-full w-full object-cover" />
+          </div>
+          <div className="row-start-5">
+            {" "}
+            <img src="asoebi8.jpeg" alt="" className="row-span-2 h-full w-full object-cover" />
+          </div>
+          <div className="col-span-2 row-start-5">
+            {" "}
+            <img src="asoebi9.jpg" alt="" className="row-span-2 h-full w-full object-cover" />
+          </div>
+          
+          
+        </div>
+          <footer className="bg-gray-500 text-white">
+            <div className="md:flex md:justify-center md:items-center sm:px-12 px-4 bg-gray-900 py-7">
+              <p> &copy; CraftsBySisi. All Rights Reserved</p>
+            </div>
+          </footer>
+        
+      </div>
+    </>
   );
-}
+};
 
 export default App;
+
